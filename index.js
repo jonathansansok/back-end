429f883b543a7c231173bfd67331db0287c0ee98
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-08-12 00:07:00 -0300

    js

---
+++ index.js
@@ -0,0 +1,39 @@
+class Usuario {
+  constructor(nombre, apellido, libros, mascotas){
+      this.nombre= nombre;
+      this.apellido = apellido;
+      this.libros = libros;
+      this.mascotas = mascotas;
+  }
+
+
+/* 
+  getfullName() */
+
+}
+
+
+
+let jonathanS = new Usuario(
+  'jonathan', 
+  'sanso',
+  [
+    barrioChino =
+    {
+      autor: 'juan sanso',
+      titulo:  'barrioChino',
+      editorial: 'Terra'
+    }
+    ,
+    cuentoDeHadas =
+    {
+      autor: 'John Smith',
+      titulo:  'cuentoDeHadas',
+      editorial: 'Las Marianas'
+    }
+  ],
+  ['perro', 'gato', 'pez']
+)
+
+
+console.log(jonathanS);
